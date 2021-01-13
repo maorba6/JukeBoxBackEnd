30f1a1efd9379bc66274ddb86d6093190e313fa1
maorba6 <you@example.commaorba6@gmail.com>
2021-01-13 20:47:03 +0200

    asdsad

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { requireAuth, requireAdmin } = require('../../middlewares/requireAuth.middleware')
-const { getUser, deleteUser } = require('./user.controller')
+const { getUser, deleteUser, updateUser } = require('./user.controller')
 const router = express.Router()
 
 
 router.get('/', getUser)
 router.delete('/:id', requireAuth, requireAdmin, deleteUser)
+router.put('/:id', updateUser, requireAuth);
 
 
 module.exports = router
