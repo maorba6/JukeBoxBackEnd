7fc1e442969d84a9967fe76b90b3407972892bac
adi <adipeled@gmail.com>
2021-01-15 10:10:33 +0200

    k

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -2,10 +2,10 @@ const express = require('express')
 const { io } = require('../../server');
 const boxService = require('../box/box.service')
 const router = express.Router()
-
+console.log(' socket routes');
 
 io.on('connection',(socket)=>{
-    console.log('connected ',socket.id);
+    console.log('io connect ',socket.id);
     socket.on('sendMsg',async (data) =>{
         box = data.currBox
         box.chat.push(data.message)
